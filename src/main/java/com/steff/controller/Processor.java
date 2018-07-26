e0455eb12175bc045e1733f0b99775166f77f4e2
Joseph <Steffin.Joseph@CenturyLink.com>
2018-07-26 15:34:59 +0530

    Added Scheduled Rest Calls

--- src/main/java/com/steff/controller/Processor.java
+++ src/main/java/com/steff/controller/Processor.java
@@ -1,13 +1,27 @@
 package com.steff.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class Processor {
 
 	@Scheduled(fixedDelay = 3000)
 	public void getDataFronServer() {
-		System.out.println("Scheduled Method Call");
+		RestTemplate restTemplate = new RestTemplate();
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ResponseEntity<String> value = restTemplate
+						.getForEntity("https://messageservicesteffin.herokuapp.com/myName/Steffin", String.class);
+				System.out.println("Scheduled Method Call : Value : " + value.getBody());
+			}
+		});
+		//t.start();
+		ResponseEntity<String> value = restTemplate
+				.getForEntity("http://messageservicesteffin.herokuapp.com/myName/Steffin", String.class);
+		System.out.println("Scheduled Method Call : Value : " + value.getBody());
 	}
 }
